a986de0b5635aef90939a9bda4f33f105a60bea6
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-03-16 22:05:30 +0530

    fix cb edit

--- src/handlers/callbacks.go
+++ src/handlers/callbacks.go
@@ -46,14 +46,14 @@ func playCallbackHandler(c *td.Client, ctx *td.Context) error {
 
 	if !cache.ChatCache.IsActive(chatID) {
 		text := "⏸ No track currently playing."
-		_ = cb.Answer(c, 300, false, text, "")
+		_ = cb.Answer(c, 1, false, text, "")
 		_, _ = cb.EditMessageText(c, text, &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons(""), ParseMode: "HTML"})
 		return nil
 	}
 
 	currentTrack := cache.ChatCache.GetPlayingTrack(chatID)
 	if currentTrack == nil {
-		_ = cb.Answer(c, 300, false, "⏸ No track currently playing.", "")
+		_ = cb.Answer(c, 1, false, "⏸ No track currently playing.", "")
 		_, _ = cb.EditMessageText(c, "⏸ No track currently playing.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons(""), ParseMode: "HTML"})
 		return nil
 	}
@@ -71,74 +71,74 @@ func playCallbackHandler(c *td.Client, ctx *td.Context) error {
 	switch {
 	case strings.Contains(data, "play_skip"):
 		if err := vc.Calls.PlayNext(chatID); err != nil {
-			_ = cb.Answer(c, 300, false, "Failed to skip track.", "")
+			_ = cb.Answer(c, 1, false, "Failed to skip track.", "")
 			_, _ = cb.EditMessageText(c, "Failed to skip track.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons(""), ParseMode: "HTML"})
 			return nil
 		}
-		_ = cb.Answer(c, 300, false, "Track skipped.", "")
+		_ = cb.Answer(c, 1, false, "Track skipped.", "")
 		_ = c.DeleteMessages(chatID, []int64{cb.MessageId}, &td.DeleteMessagesOpts{Revoke: true})
 		return nil
 
 	case strings.Contains(data, "play_stop"):
 		if err := vc.Calls.Stop(chatID); err != nil {
-			_ = cb.Answer(c, 300, false, "Failed to stop track.", "")
+			_ = cb.Answer(c, 1, false, "Failed to stop track.", "")
 			_, _ = cb.EditMessageText(c, "Failed to stop track.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons(""), ParseMode: "HTML"})
 			return nil
 		}
 		msg := fmt.Sprintf("⏹ <b>Playback Stopped</b>\n└ Requested by: %s", user.FirstName)
-		_ = cb.Answer(c, 300, false, "Playback stopped.", "")
+		_ = cb.Answer(c, 1, false, "Playback stopped.", "")
 		_, err := cb.EditMessageText(c, msg, &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons(""), ParseMode: "HTML"})
 		return err
 
 	case strings.Contains(data, "play_pause"):
 		if _, err = vc.Calls.Pause(chatID); err != nil {
-			_ = cb.Answer(c, 300, false, "Failed to pause track.", "")
+			_ = cb.Answer(c, 1, false, "Failed to pause track.", "")
 			_, _ = cb.EditMessageText(c, "Failed to pause track.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons(""), ParseMode: "HTML"})
 			return nil
 		}
-		_ = cb.Answer(c, 300, false, "Track paused.", "")
+		_ = cb.Answer(c, 1, false, "Track paused.", "")
 		text := buildTrackMessage("Paused", "⏸") + fmt.Sprintf("\n\n⏸ <i>Paused by %s</i>", user.FirstName)
 		_, _ = cb.EditMessageText(c, text, &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("pause"), ParseMode: "HTML"})
 		return nil
 
 	case strings.Contains(data, "play_resume"):
 		if _, err := vc.Calls.Resume(chatID); err != nil {
-			_ = cb.Answer(c, 300, false, "Failed to resume track.", "")
+			_ = cb.Answer(c, 1, false, "Failed to resume track.", "")
 			_, _ = cb.EditMessageText(c, "Failed to resume track.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("pause"), ParseMode: "HTML"})
 			return nil
 		}
 
-		_ = cb.Answer(c, 300, false, "Track resumed.", "")
+		_ = cb.Answer(c, 1, false, "Track resumed.", "")
 		text := buildTrackMessage("Now Playing", "🎵") + fmt.Sprintf("\n\n▶️ <i>Resumed by %s</i>", user.FirstName)
 		_, _ = cb.EditMessageText(c, text, &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("resume"), ParseMode: "HTML"})
 		return nil
 
 	case strings.Contains(data, "play_mute"):
 		if _, err := vc.Calls.Mute(chatID); err != nil {
-			_ = cb.Answer(c, 300, false, "Failed to mute track.", "")
+			_ = cb.Answer(c, 1, false, "Failed to mute track.", "")
 			_, _ = cb.EditMessageText(c, "Failed to mute track.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("mute"), ParseMode: "HTML"})
 			return nil
 		}
 
-		_ = cb.Answer(c, 300, false, "Track muted.", "")
+		_ = cb.Answer(c, 1, false, "Track muted.", "")
 		text := buildTrackMessage("Muted", "🔇") + fmt.Sprintf("\n\n🔇 <i>Muted by %s</i>", user.FirstName)
 		_, _ = cb.EditMessageText(c, text, &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("mute"), ParseMode: "HTML"})
 		return nil
 
 	case strings.Contains(data, "play_unmute"):
 		if _, err := vc.Calls.Unmute(chatID); err != nil {
-			_ = cb.Answer(c, 300, false, "Failed to unmute track.", "")
+			_ = cb.Answer(c, 1, false, "Failed to unmute track.", "")
 			_, _ = cb.EditMessageText(c, "Failed to unmute track.", &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("unmute"), ParseMode: "HTML"})
 			return nil
 		}
-		_ = cb.Answer(c, 300, false, "Track unmuted.", "")
+		_ = cb.Answer(c, 1, false, "Track unmuted.", "")
 		text := buildTrackMessage("Now Playing", "🎵") + fmt.Sprintf("\n\n🔊 <i>Unmuted by %s</i>", user.FirstName)
 		_, _ = cb.EditMessageText(c, text, &td.EditTextMessageOpts{ReplyMarkup: core.ControlButtons("unmute"), DisableWebPagePreview: true})
 		return nil
 	case strings.Contains(data, "play_add_to_list"):
 		playlists, err := db.Instance.GetUserPlaylists(ctx2, cb.SenderUserId)
 		if err != nil {
-			_ = cb.Answer(c, 300, false, "An error occurred while fetching playlists.", "")
+			_ = cb.Answer(c, 1, false, "An error occurred while fetching playlists.", "")
 			return nil
 		}
 
@@ -146,7 +146,7 @@ func playCallbackHandler(c *td.Client, ctx *td.Context) error {
 		if len(playlists) == 0 {
 			playlistID, err = db.Instance.CreatePlaylist(ctx2, "My Playlist (TgMusic)", cb.SenderUserId)
 			if err != nil {
-				_ = cb.Answer(c, 300, false, "An error occurred while creating a new playlist.", "")
+				_ = cb.Answer(c, 1, false, "An error occurred while creating a new playlist.", "")
 				return nil
 			}
 		} else {
@@ -163,17 +163,17 @@ func playCallbackHandler(c *td.Client, ctx *td.Context) error {
 
 		err = db.Instance.AddSongToPlaylist(ctx2, playlistID, song)
 		if err != nil {
-			_ = cb.Answer(c, 300, false, "An error occurred while adding the track to the playlist.", "")
+			_ = cb.Answer(c, 1, false, "An error occurred while adding the track to the playlist.", "")
 			return nil
 		}
 
 		playlist, err := db.Instance.GetPlaylist(ctx2, playlistID)
 		if err != nil {
-			_ = cb.Answer(c, 300, false, "Playlist not found :)", "")
+			_ = cb.Answer(c, 1, false, "Playlist not found :)", "")
 			return nil
 		}
 
-		_ = cb.Answer(c, 300, false, fmt.Sprintf("✅ '%s' has been added to the playlist '%s'.", song.Name, playlist.Name), "")
+		_ = cb.Answer(c, 1, false, fmt.Sprintf("✅ '%s' has been added to the playlist '%s'.", song.Name, playlist.Name), "")
 		return nil
 	}
 
@@ -188,7 +188,7 @@ func vcPlayHandler(c *td.Client, ctx *td.Context) error {
 	cb := ctx.Update.UpdateNewCallbackQuery
 	data := cb.DataString()
 	if strings.Contains(data, "vcplay_close") {
-		_ = cb.Answer(c, 300, false, "Closing...", "")
+		_ = cb.Answer(c, 1, false, "Closing...", "")
 		_ = c.DeleteMessages(cb.ChatId, []int64{cb.MessageId}, &td.DeleteMessagesOpts{Revoke: true})
 		return nil
 	}
